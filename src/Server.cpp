33c7cb5251f41a84e17356776e3700eff5869050
Ash <01tripster@gmail.com>
2025-01-29 18:13:14 +0530

    cat file command

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,35 +2,34 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <set>
+#include "zstr.hpp"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // Flush after every std::cout / std::cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!\n";
-
-    // Uncomment this block to pass the first stage
+    // Flush after every cout / cerr
+    cout << unitbuf;
+    cerr << unitbuf;
 
     if (argc < 2)
     {
-        std::cerr << "No command provided.\n";
+        cerr << "No command provided.\n";
         return EXIT_FAILURE;
     }
 
-    std::string command = argv[1];
+    string command = argv[1];
 
     if (command == "init")
     {
         try
         {
-            std::filesystem::create_directory(".git");
-            std::filesystem::create_directory(".git/objects");
-            std::filesystem::create_directory(".git/refs");
+            filesystem::create_directory(".git");
+            filesystem::create_directory(".git/objects");
+            filesystem::create_directory(".git/refs");
 
-            std::ofstream headFile(".git/HEAD");
+            ofstream headFile(".git/HEAD");
             if (headFile.is_open())
             {
                 headFile << "ref: refs/heads/main\n";
@@ -38,21 +37,69 @@ int main(int argc, char *argv[])
             }
             else
             {
-                std::cerr << "Failed to create .git/HEAD file.\n";
+                cerr << "Failed to create .git/HEAD file.\n";
                 return EXIT_FAILURE;
             }
 
-            std::cout << "Initialized git directory\n";
+            cout << "Initialized git directory\n";
         }
-        catch (const std::filesystem::filesystem_error &e)
+        catch (const filesystem::filesystem_error &e)
         {
-            std::cerr << e.what() << '\n';
+            cerr << e.what() << '\n';
             return EXIT_FAILURE;
+        }
+    }
+    else if (command == "cat-file")
+    {
+        if (argc <= 3)
+        {
+            cerr << "Invalid arguments, required `-p <blob_sha>`\n";
+            return EXIT_FAILURE;
+        }
+
+        const string flag = argv[2];
+
+        // Define supported flags
+        const set<string> supported_flags = {"-p"};
+
+        // Check if the provided flag is supported
+        if (supported_flags.find(flag) == supported_flags.end())
+        {
+            cerr << "Invalid flag for cat-file, expected one of ";
+            for (const auto &f : supported_flags)
+            {
+                cerr << f << " ";
+            }
+            cerr << "\n";
+            return EXIT_FAILURE;
+        }
+
+        if (flag == "-p")
+        {
+            const string value = argv[3];
+            const string dir_name = value.substr(0, 2);
+            const string blob_sha = value.substr(2);
+
+            string path = ".git/objects/" + dir_name + "/" + blob_sha;
+
+            zstr::ifstream input(path, ios::binary);
+            if (!input.is_open())
+            {
+                cerr << "Failed to open object file.\n";
+                return EXIT_FAILURE;
+            }
+
+            string object_str{istreambuf_iterator<char>(input),
+                              istreambuf_iterator<char>()};
+            input.close();
+
+            const auto object_content = object_str.substr(object_str.find('\0') + 1);
+            cout << object_content << flush;
         }
     }
     else
     {
-        std::cerr << "Unknown command " << command << '\n';
+        cerr << "Unknown command " << command << '\n';
         return EXIT_FAILURE;
     }
 
